44b137d5964d141d2b61666579e88580ebff22bf
katrina gardner <yutzeng0314@gmail.com>
2015-02-06 15:47:56 -0500

    rails

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+Post.create(headline: "Pizza Shop", title: "Morino", content: "Best veggie pizza in NYC.")
+
+Post.create(headline: "Cookie Shop", title: "ButterMilk", content: "Best cookies in NYC.")
+
+
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
